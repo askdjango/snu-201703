a70cc347cd220caa1e522ebce5c812a4144ee20f
Chinseok Lee <allieuslee@gmail.com>
2017-03-15 16:23:23 +0900

    URLConf 정규표현식 매칭 예제

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def post_list(request):
     return render(request, 'blog/post_list.html')
+
+def post_detail(request, id):
+    return HttpResponse('{}번글을 보시겠습니다.'.format(id))
+
