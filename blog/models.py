3cec5cddf75f0f42d74b35cbfa925208b6c15b02
Chinseok Lee <allieuslee@gmail.com>
2017-03-30 09:55:35 +0900

    blog.models.Post 모델에 필수 필드 author 추가하고, 마이그레이션

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Post(models.Model):
+    author = models.CharField(max_length=20)
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
