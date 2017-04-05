8346cec1cb2418d395a577c25f9823e61089f411
Chinseok Lee <allieuslee@gmail.com>
2017-04-05 16:40:32 +0900

    dojo.models.Post 모델에 대한 커스텀 admin

--- dojo/admin.py
+++ dojo/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Post
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content_size', 'tags', 'lnglat']
+    # list_display_links = ['title']
+    list_editable = ['title']
+    search_fields = ['title']
+    list_filter = ['created_at']
 
-admin.site.register(Post)
+    def content_size(self, post):
+        html = '<span style="color: blue;">{}</span>글자'.format(len(post.content))
+        return mark_safe(html)
+    content_size.short_description = '글자수'
+    # content_size.allow_tags = True  # deprecated
+
+admin.site.register(Post, PostAdmin)
 
