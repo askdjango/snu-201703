09b5c24c9cfd4e119ba9579d835172e9c92701a9
Chinseok Lee <allieuslee@gmail.com>
2017-04-05 14:48:20 +0900

    dojo앱에 Post모델을 추가하고, admin에 등록

--- dojo/models.py
+++ dojo/models.py
@@ -1,3 +1,17 @@
+import re
+from django import forms
 from django.db import models
 
-# Create your models here.
+def lnglat_validator(value):
+    if not re.match(r'^(\d+\.?\d*),(\d+\.?\d*)$', value):
+        raise forms.ValidationError('Invalid LngLat Value')
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    tags = models.CharField(max_length=100, blank=True)
+    lnglat = models.CharField(max_length=50,
+            validators=[lnglat_validator])
+    created_at = models.DateTimeField(auto_now_add=True)
+    test_field = models.IntegerField(default=10)
+
