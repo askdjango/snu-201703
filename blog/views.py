597c8cec7b04323b210c5a62150364519e000573
Chinseok Lee <allieuslee@gmail.com>
2017-04-19 14:25:56 +0900

    blog.views.post_list 뷰에서 Post 목록 구현

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html')
+    post_list = Post.objects.all()
+    return render(request, 'blog/post_list.html', {
+        'post_list': post_list,
+    })
+
+
 
 def post_detail(request, id):
     return HttpResponse('{}번글을 보시겠습니다.'.format(id))
