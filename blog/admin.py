440807f88ecc3c8f823015ca930b2b6c40c5e0e1
Chinseok Lee <allieuslee@gmail.com>
2017-04-19 14:10:40 +0900

    blog.admin.PostAdmin 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'author']
+    list_display_links = ['title']
