1f5eec6f5c4a42875fc3e0d4c4079d55fe1df707
Chinseok Lee <allieuslee@gmail.com>
2017-04-19 15:14:34 +0900

    blog.models.Post 모델에 기본정렬조건 추가

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,6 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['-id']
+
