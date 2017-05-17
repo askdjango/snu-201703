6009a0c77f14c48354d7c12a56cfb43a3e5ab1a7
Chinseok Lee <allieuslee@gmail.com>
2017-05-17 14:57:09 +0900

    blog.models.Post 모델에 user 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,9 @@
+from django.conf import settings
 from django.db import models
 
 
 class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
     author = models.CharField(max_length=20)
     title = models.CharField(max_length=100)
     content = models.TextField()
