ac377c05b96d6813e74a681ae4a9b2e9b6e9466b
Chinseok Lee <allieuslee@gmail.com>
2017-05-17 14:25:41 +0900

    accounts.Profile 모델 생성/등록

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+# from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField('auth.User')
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+
