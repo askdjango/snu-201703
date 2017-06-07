b0d5d5586047fc023b930f66fc03a2d4a6a03460
Chinseok Lee <allieuslee@gmail.com>
2017-06-07 15:15:57 +0900

    회원가입 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     url(r'^login/$', auth_views.login, name='login'),
+    url(r'^signup/$', views.signup, name='signup'),
 ]
 
