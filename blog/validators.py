299c63e463eb7045fe4f0686740bcbf6ca760f7f
Chinseok Lee <allieuslee@gmail.com>
2017-06-07 14:31:42 +0900

    min_length validator 함수 구현을 클래스 모듈로 대체

--- blog/validators.py
+++ blog/validators.py
@@ -1,7 +1,10 @@
 from django import forms
+from django.core.validators import MinLengthValidator
 
 
-def min_length_3_validator(value):
-    if len(value) < 3:
-        raise forms.ValidationError('3글자 이상 입력해주세요.')
+# def min_length_3_validator(value):
+#     if len(value) < 3:
+#        raise forms.ValidationError('3글자 이상 입력해주세요.')
+
+min_length_3_validator = MinLengthValidator(3)
 
